af1bb10f48e79ed1cbf1256b51435ba4cefb7a3d
miura-reo <61036722+miura-reo@users.noreply.github.com>
2021-01-20 13:27:17 +0900

    [Fix]Change the argument of delete_post_related_images.
    Passing instances directly.

--- piedpiper_web/cms/views.py
+++ piedpiper_web/cms/views.py
@@ -105,7 +105,7 @@ def techblog_del(request, techblog_id):
     # 記事の削除
     techblog = get_object_or_404(Techblog, pk=techblog_id)
     qiita = Qiita()
-    qiita.delete_post_related_images(techblog.qiita_item_id)
+    qiita.delete_post_related_images(techblog)
     techblog.delete()
     return redirect('cms:techblog_list')
 
