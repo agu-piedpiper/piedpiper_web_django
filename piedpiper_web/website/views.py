ee2ca18bc1ec180bb95dfefe8299733778b0e9db
kouhei25k <kouhei25k@gmail.com>
2021-01-29 17:11:53 +0900

    add techblog frontend

--- piedpiper_web/website/views.py
+++ piedpiper_web/website/views.py
@@ -4,6 +4,7 @@ import re
 # Create your views here.
 def index(request):
     activities = Activity.objects.all().order_by('id')[0:3]
+    techblogs = Techblog.objects.all().order_by('id')[0:3]
     p = re.compile(r"<[^>]*?>")
     for activity in activities:
         activity.body = activity.body.replace("　", "").replace("\n", "")
@@ -11,7 +12,7 @@ def index(request):
        
         activity.published_at = activity.published_at.strftime("%Y/%m/%d")
        
-    return render(request, 'website/index.html',{'activities': activities})
+    return render(request, 'website/index.html',{'activities': activities},{'techblogs':techblogs})
 
 def activity_list(request):
 
@@ -28,3 +29,21 @@ def activity_list(request):
 def activity_detail(request,request_id):    
     activity = Activity.objects.get(id=request_id)
     return render(request, 'website/activity_detail.html',{'activity': activity})
+
+
+
+def techblog_list(request):
+
+    techblogs = Techblog.objects.all().order_by('id')
+    p = re.compile(r"<[^>]*?>")
+    for techblog in techblogs:
+        techblog.body = techblog.body.replace("　", "").replace("\n", "")
+        techblog.body =p.sub("", techblog.body)
+        techblog.published_at = techblog.published_at.strftime("%Y/%m/%d")
+       
+    return render(request, 'website/techblog.html',{'activities': activities})
+
+
+def techblog_detail(request,request_id):    
+    techblog = techblog.objects.get(id=request_id)
+    return render(request, 'website/techblog_detail.html',{'techblog': techblog})
