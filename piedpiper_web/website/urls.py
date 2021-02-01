5b47d316e5b8569bf688d9e5135202e01504d263
kouhei25k <kouhei25k@gmail.com>
2021-02-02 04:48:35 +0900

    fix dir

--- piedpiper_web/website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'website'
-urlpatterns = [
-    path('', views.index, name='index'), 
-    path('activity/', views.activity_list, name='activity_list'), 
-    path('activity/<int:request_id>/', views.activity_detail, name='activity_detail'), 
-]
